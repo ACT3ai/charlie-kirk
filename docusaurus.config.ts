b347ee8f579815acd4d86d8156f24cff322e917d
rafin-whitehat <99549928+rafin-whitehat@users.noreply.github.com>
2025-10-14 13:35:00 +0545

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://act3ai.github.io',
+  url: 'https://whoassassinatedcharliekirk.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/charlie-kirk/',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ACT3ai', // Usually your GitHub org/user name.
+  projectName: 'charlie-kirk', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
