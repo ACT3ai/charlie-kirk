94f7bab012818396a2ce5e11f3d474899d9570a8
Bryan Starbuck <Bryan@thestarbucks.com>
2026-06-27 22:56:33 -0700

    Bryan 27 Laptop

--- site/sidebars.ts
+++ site/sidebars.ts
@@ -37,6 +37,7 @@ const sidebars: SidebarsConfig = {
     {type: 'doc', id: 'maps/overview', label: 'Maps'},
     {type: 'doc', id: 'US_Intelligence/overview', label: 'U.S. Intelligence'},
     {type: 'doc', id: 'Photos/overview', label: 'Photos'},
+    {type: 'doc', id: 'cameras/overview', label: 'Surveillance Cameras'},
     {type: 'doc', id: 'Censorship/overview', label: 'Censorship'},
     {type: 'doc', id: 'FBI/overview', label: 'FBI'},
     {type: 'doc', id: 'Locations/overview', label: 'Locations'},
